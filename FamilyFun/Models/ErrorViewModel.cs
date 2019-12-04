b8a772a8f434a01c8a54e6006bef3274eeff8754
Shmuel YArmush <slimsheivy@aol.com>
2019-12-04 18:46:20 -0500

    Add project files.

---
+++ FamilyFun/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FamilyFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
