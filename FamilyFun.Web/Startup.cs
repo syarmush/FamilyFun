28f458e66d36ba79b36f851f2362544e13607612
SY <sy@sy.com>
2021-02-11 18:53:57 -0500

    add mitzvos for family members

--- FamilyFun.Web/Startup.cs
+++ FamilyFun.Web/Startup.cs
@@ -109,8 +109,8 @@ namespace FamilyFun.Web
                     {
                         [1] = new int[] { 1 },
                         [2] = new int[] { 1 },
-                        [3] = new int[] { 1,2 },
-                        [4] = new int[] { 1,2 },
+                        [3] = new int[] { 1,2,3,4,5,6,7,8,9,10,11 },
+                        [4] = new int[] { 1,2,3,4,5,6,7,8,9,10,11 },
                     },
                     s.GetService<IMitzvahImageDirectoryPathRetriever>());
             });
